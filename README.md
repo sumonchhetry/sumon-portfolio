4b65ea00ea4ed69f799d58c62612b2ac6a2696c4
Sumon Chhetry <{ID}+{username}@users.noreply.github.com>
2024-01-08 23:06:09 +0530

    initial-setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sumon-portfolio
+
 a protfolio website using html, css and js
+
+# navbar section written
---
+++ app.js
@@ -0,0 +1,10 @@
+// 'use strict';
+
+// const darkBtn = document.querySelector('#dark-btn');
+
+// darkBtn.addEventListener('click', function (e) {
+//   document.body.style.backgroundColor = '#333';
+//   document.innerHTML.style.backgroundColor = '#fff';
+
+//   e.preventDefault();
+// });
